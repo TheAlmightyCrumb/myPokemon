290babb6ec985efbb1e66508f45b2e5260f6a902
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-08-03 16:22:29 +0300

    Re-Initializing

--- main.js
+++ main.js
@@ -1,26 +1,8 @@
-const searchBtn = document.getElementById('searchButton');
-const input = document.getElementById('search');
+const input = document.getElementById('searchLine');
 const pokemonDiv = document.getElementById('results');
-
-const searchPokemon = async (pokemonId = 3) => {
-  const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  makeDiv(data.name, data.height, data.weight, data.sprites.front_default);
-};
-
-const makeDiv = (name, height, weight, picture) => {
-  const htmlText = `
-    <div class="pokemonContainer">
-      <div>Name: ${name}</div>
-      <div>height: ${height}</div>
-      <div>weight: ${weight}</div>
-      <div>picture: <img src="${picture}" /></div>
-    </div>
-  `;
-  pokemonDiv.innerHTML = htmlText;
-}
-
-searchBtn.onclick = function() {
-    searchPokemon(input.value);
-}
-
-
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonHeight = document.getElementById('pokemon-height');
+const pokemonWeight = document.getElementById('pokemon-weight');
+const pokemonImg = document.getElementById('pokemon-image');
+const listNames = document.getElementById('types');
+const pokemonList = document.getElementById('list');
