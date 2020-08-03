692a6aa932ddce629c5ada9b4505c3c88df3eba1
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-08-03 16:07:49 +0300

    Search and show details of pokemon

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+const searchBtn = document.getElementById('searchButton');
+const input = document.getElementById('search');
+const pokemonDiv = document.getElementById('results');
+
 const searchPokemon = async (pokemonId = 3) => {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
+  makeDiv(data.name, data.height, data.weight, data.sprites.front_default);
 };
-searchPokemon();
+
+const makeDiv = (name, height, weight, picture) => {
+  const htmlText = `
+    <div class="pokemonContainer">
+      <div>Name: ${name}</div>
+      <div>height: ${height}</div>
+      <div>weight: ${weight}</div>
+      <div>picture: <img src="${picture}" /></div>
+    </div>
+  `;
+  pokemonDiv.innerHTML = htmlText;
+}
+
+searchBtn.onclick = function() {
+    searchPokemon(input.value);
+}
+
 
